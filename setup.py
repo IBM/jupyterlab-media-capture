ba7ce7aba6476d76a017cccce929ffc4527c17a2
James Reeve <james.reeve@ibm.com>
2020-04-04 15:45:20 -0400

    0.2.7

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 data_files_spec = [
-    ('etc/jupyter/jupyter_notebook_config.d',
-     'jupyter-config/jupyter_notebook_config.d',
-     'jupyterlab_media_capture.json'),
+    (
+        "etc/jupyter/jupyter_notebook_config.d",
+        "jupyter-config/jupyter_notebook_config.d",
+        "jupyterlab_media_capture.json",
+    ),
 ]
 
 setup_dict = dict(
-    name='jupyterlab_media_capture',
+    name="jupyterlab_media_capture",
     packages=find_packages(),
-    author          = 'James Reeve',
-    author_email    = 'james.reeve@ibm.com',
-    install_requires=[
-        'notebook'
-    ]
+    author="James Reeve",
+    author_email="james.reeve@ibm.com",
+    install_requires=["notebook"],
 )
 
-setup(
-    version='0.2.6',
-    **setup_dict
-)
+setup(version="0.2.7", **setup_dict)
